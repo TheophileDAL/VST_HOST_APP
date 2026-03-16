d5abe166aa6e63a3cfab78dcb445423b33169d76
theophiledal <theophile.dal@efrei.net>
2026-03-16 18:04:07 +0100

    Initial Commit

---
+++ VST HOST/VST_HOSTApp.swift
@@ -0,0 +1,17 @@
+//
+//  VST_HOSTApp.swift
+//  VST HOST
+//
+//  Created by Théophile Dal on 16/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct VST_HOSTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
